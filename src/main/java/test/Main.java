b46ea0cb6f9a9db25e70bc57864063b394b953b4
Andrey Pomelov <test.maven@ya.ru>
2022-11-15 20:36:08 +0400

    Initial commit

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.apache.log4j.net.SocketAppender;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SocketAppender appender = new SocketAppender();
+    }
+}
